6f7cf8475a8929611307b1986ce134fa26716a62
Nick Stein <nstein@nicarus>
2016-03-11 12:14:16 -0500

    Added support for listing all instances

--- aws-enter.js
+++ aws-enter.js
@@ -11,41 +11,71 @@
     var opts = require("nomnom").option('name', {
         abbr: 'n', 
         help: 'This option selects a single AWS EC2 instance by it\'s `Name` Tag.'
+    }).option('list', {
+        flag: true,
+        abbr: 'l',
+        help: 'This option displays a list of all available EC2 instances by name.'
     }).parse();
-    
+
     // Load config file
-    var config = JSON.parse(fs.readFileSync(path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], '.awsenter'), { encoding: "ascii" }) || null); 
+    var configFilePath = path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], '.awsenter');
+    var configFile = fs.readFileSync(configFilePath, { encoding: "ascii" });
+    var config = JSON.parse(configFile || null); 
     // Main logic
     var app = {
-        run: function (o) {
-            if (o.request == undefined) { console.log(HELP_TEXT); process.exit(); }
+        run: function (options) {
+            if (options.request == undefined) { 
+                console.log(HELP_TEXT); 
+                process.exit(); 
+            }
             // Load AWS root key via options object
-            AWS.config.update({ accessKeyId: o.config.access_key, secretAccessKey: o.config.secret_access_key, region: o.config.region });
-            
+            AWS.config.update({ 
+                accessKeyId: options.config.access_key, 
+                secretAccessKey: options.config.secret_access_key, 
+                region: options.config.region 
+            });
+
             // Initialize EC2
             var ec2 = new AWS.EC2();
 
             ec2.describeInstances({
-                Filters: [ { Name: 'tag:Name', Values: [ o.request ] } ],
-            }, function (e, d) {
-                if (e) {
-                    console.log(e); // an error occurred
+                Filters: [ options.request.list ? null : { 
+                    Name: 'tag:Name', 
+                    Values: [ options.request.name ] 
+                } ],
+            }, function (error, query) {
+                if (error) {
+                    console.log(error); // an error occurred
                 } else {
-                    
+
                     // Grab a list of all the hosts' private IP addresses, public IP addresses, and tags. 
                     // This can be easily expanded to support multiple IP output, searching by multiple tags and/or multiple tag values, etc.
                     var hosts = _.unique(
-                        _.flatten(
-                            _.map(d.Reservations, function (x) {
-                                return _.map(x.Instances, function (y) {
-                                    return { PublicIpAddress: y.PublicIpAddress, PrivateIpAddress: y.PrivateIpAddress, Tags: y.Tags };
-                                });
-                            })
-                        )
-                    );
-                    
+                            _.flatten(
+                                _.map(query.Reservations, function (reservation) {
+                                    return _.map(reservation.Instances, function (instance) {
+                                        return { 
+                                            PublicIpAddress: instance.PublicIpAddress, 
+                                            PrivateIpAddress: instance.PrivateIpAddress, 
+                                            Tags: instance.Tags
+                                        };
+                                    });
+                                })
+                                )
+                            );
+
                     // For now, just output the IP address of each result on a line.
-                    _.each(hosts, function (z) { console.log(z.PublicIpAddress); });
+                    _.each(hosts, function (host) { 
+                        // Handle simple lookup request: 
+                        if (options.request.name != null) {
+                            console.log(host.PublicIpAddress);
+                            return;
+                        }
+                        // Otherwise this is a list request - show more information
+                        var name = _.findWhere(host.Tags, { Key: "Name" }) || null;
+                        name = name === null ? "[unnamed instance]" : name.Value;
+                        console.log("Name: " + name + " | " + "Public IP: " + host.PublicIpAddress); 
+                    });
                 }
             });
         }
@@ -53,7 +83,7 @@
 
     app.run({
         config: config,
-        request: opts.name
+        request: opts
     });
 
 })();
